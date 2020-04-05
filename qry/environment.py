4191d99c81a0d8363b94da349f2e9135c2f58124
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-04-05 14:00:53 +0100

    Add parent ptr to environment

--- qry/environment.py
+++ qry/environment.py
@@ -1,11 +1,12 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 from dataclasses import dataclass
 
 @dataclass
 class Environment:
 	name: str
 	state: Dict[str, Any]
+	parent: Optional['Environment'] = None
 
 	def child_env(self, name: str) -> 'Environment':
 		child_state = self.state.copy()
-		return Environment(name, child_state)
+		return Environment(name, child_state, self)
