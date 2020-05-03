e6a68d87ee8a4473ffc0fc50d8219343342d265d
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-05-03 10:37:00 +0100

    Unify database tests

--- tests/test_stdlib.py
+++ tests/test_stdlib.py
@@ -16,34 +16,6 @@ meta_exprs = [
 
 test_meta_lib = data_driven_test(meta_exprs)
 
-table_bootstrap = '''
-attach(data)
-
-conn <- connect_sqlite(":memory:")
-execute(conn, "create table my_table (name text, age integer)")
-execute(conn, "insert into my_table(name, age) values ('ruan', 26), ('ruanlater', 27), ('thirdperson', 27)")
-'''
-
-data_exprs = [
-	(table_bootstrap + '''
-	get_table(conn, "my_table")
-		|> filter(age <= 26)
-		|> count_rows()
-	''', 1),
-	(table_bootstrap + '''
-	get_table(conn, "my_table")
-		|> aggregate(group(age), total_years = sum(age))
-		|> count_rows()
-	''', 2),
-	(table_bootstrap + '''
-	get_table(conn, "my_table")
-		|> cross_join(get_table(conn, "my_table"))
-		|> count_rows()
-	''', 9),
-]
-
-test_data_lib = data_driven_test(data_exprs)
-
 op_exprs = [
 	('to_string(true)', 'true'),
 	('to_string(1 + 1)', '2'),
--- tests/test_stdlib_data.py
+++ tests/test_stdlib_data.py
@@ -1,43 +1,41 @@
 from uuid import uuid4
 import os
-
-import pytest
+from typing import Any, List, Tuple, Optional
 
 from .eval_helpers import data_driven_test
-
-if os.getenv('QRY_CI_NODOCKER'):
-	pytest.skip('skipping tests requiring docker', allow_module_level = True)
-
-connect = '''
-attach(data)
-conn <- connect_postgres("localhost", 54321, "qrytest", "qry", "password")
-'''
 
 run_id = str(uuid4()).replace('-', '')
 
 def table_name(name: str) -> str:
 	return f'qry_{run_id}_{name}'
 
-postgres_exprs = [
-	(connect + f'''
+data_exprs = [
+	(f'''
 	execute(conn, "create table {table_name('my_table')} (name text, age integer)")
 	execute(conn, "insert into {table_name('my_table')}(name, age) values ('ruan', 26), ('ruanlater', 27), ('thirdperson', 27)")
 	''', None),
-	(connect + f'''
+	(f'''
 	get_table(conn, "{table_name('my_table')}")
 		|> filter(age <= 26)
 		|> count_rows()
 	''', 1),
-	(connect + f'''
+	(f'''
 	get_table(conn, "{table_name('my_table')}")
 		|> aggregate(group(age), total_years = sum(age))
 		|> count_rows()
 	''', 2),
-	(connect + f'''
+	(f'''
 	get_table(conn, "{table_name('my_table')}")
 		|> cross_join(get_table(conn, "{table_name('my_table')}"))
 		|> count_rows()
 	''', 9),
 ]
 
-test_postgres = data_driven_test(postgres_exprs)
+def data_test(connect_code: str) -> Any:
+	tests = [('attach(data) ' + connect_code + code, expectation) for code, expectation in data_exprs]
+	return data_driven_test(tests)
+
+test_sqlite = data_test('conn <- connect_sqlite("tests/artefacts/db.sqlite")')
+
+if not os.getenv('QRY_CI_NODOCKER'):
+	test_postgres = data_test('conn <- connect_postgres("localhost", 54321, "qrytest", "qry", "password")')
